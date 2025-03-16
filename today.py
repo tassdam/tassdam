d84d6d8498a1f5c58a4c2d2a6fd9fc17d912b602
Damir Tassybayev <tassybayev.kostanay@gmail.com>
2025-03-16 01:18:35 +0100

    update 4.0

--- today.py
+++ today.py
@@ -254,15 +254,15 @@ def stars_counter(data):
 def svg_overwrite(filename, age_data, commit_data, star_data, repo_data, contrib_data, follower_data, loc_data):
     tree = etree.parse(filename)
     root = tree.getroot()
-    justify_format(root, 'age_data', age_data)
-    justify_format(root, 'commit_data', commit_data, 22)
-    justify_format(root, 'star_data', star_data, 14)
+    justify_format(root, 'age_data', age_data, 42)
+    justify_format(root, 'commit_data', commit_data, 12)
+    justify_format(root, 'star_data', star_data, 6)
     justify_format(root, 'repo_data', repo_data, 6)
     justify_format(root, 'contrib_data', contrib_data)
-    justify_format(root, 'follower_data', follower_data, 10)
-    justify_format(root, 'loc_data', loc_data[2], 9)
+    justify_format(root, 'follower_data', follower_data, 12)
+    justify_format(root, 'loc_data', loc_data[2], 2)
     justify_format(root, 'loc_add', loc_data[0])
-    justify_format(root, 'loc_del', loc_data[1], 7)
+    justify_format(root, 'loc_del', loc_data[1])
     tree.write(filename, encoding='utf-8', xml_declaration=True)
 
 
